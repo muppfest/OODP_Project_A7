e2c4e3210a4f095022382734bd2253d3b64e6994
muppfest <marcus.vretling.pistelli@gmail.com>
2018-10-19 15:36:21 +0200

    Momentklass tillagd

---
+++ OODP_Projekt_A7/src/model/Moment.java
@@ -0,0 +1,12 @@
+package model;
+
+import java.sql.Date;
+
+public class Moment {
+	private String code;
+	private String type;
+	private String description;
+	private String grade;
+	private Date date;
+	private double credit;
+}
