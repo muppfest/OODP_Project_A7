ac973bfc7c346dc58a2130c7198b93f33f49a4f1
muppfest <marcus.vretling.pistelli@gmail.com>
2018-10-19 15:41:11 +0200

    subklasser till moment inlagda

--- OODP_Projekt_A7/src/model/Moment.java
+++ OODP_Projekt_A7/src/model/Moment.java
@@ -9,4 +9,43 @@ public class Moment {
 	private String grade;
 	private Date date;
 	private double credit;
+	
+	public String getCode() {
+		return code;
+	}
+	public void setCode(String code) {
+		this.code = code;
+	}
+	public String getType() {
+		return type;
+	}
+	public void setType(String type) {
+		this.type = type;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public String getGrade() {
+		return grade;
+	}
+	public void setGrade(String grade) {
+		this.grade = grade;
+	}
+	public Date getDate() {
+		return date;
+	}
+	public void setDate(Date date) {
+		this.date = date;
+	}
+	public double getCredit() {
+		return credit;
+	}
+	public void setCredit(double credit) {
+		this.credit = credit;
+	}
+	
+	
 }
