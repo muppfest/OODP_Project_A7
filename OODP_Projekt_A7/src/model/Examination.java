71a55205f713f405ddcc5cc6a9114a60a0bf1dd7
muppfest <marcus.vretling.pistelli@gmail.com>
2018-10-19 15:46:56 +0200

    examination class added

---
+++ OODP_Projekt_A7/src/model/Examination.java
@@ -0,0 +1,21 @@
+package model;
+
+import java.util.Date;
+
+public class Examination extends Moment {
+	private Date lastRegisterDate;
+	private String place;
+	
+	public Date getLastRegisterDate() {
+		return lastRegisterDate;
+	}
+	public void setLastRegisterDate(Date lastRegisterDate) {
+		this.lastRegisterDate = lastRegisterDate;
+	}
+	public String getPlace() {
+		return place;
+	}
+	public void setPlace(String place) {
+		this.place = place;
+	}
+}
